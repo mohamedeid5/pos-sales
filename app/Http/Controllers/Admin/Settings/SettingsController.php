8c943a973b44a9c9173f16d310df91bd9e6105db
mohamed eid <meid368@gmail.com>
2023-05-20 15:39:33 +0300

    update settings

--- app/Http/Controllers/Admin/Settings/SettingsController.php
+++ app/Http/Controllers/Admin/Settings/SettingsController.php
@@ -25,30 +25,6 @@ class SettingsController extends Controller
     }
 
     /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     */
-    public function store(Request $request)
-    {
-        //
-    }
-
-    /**
-     * Display the specified resource.
-     */
-    public function show(string $id)
-    {
-        //
-    }
-
-    /**
      * Show the form for editing the specified resource.
      */
     public function edit()
@@ -64,11 +40,4 @@ class SettingsController extends Controller
         return $this->settingsRepository->update($request);
     }
 
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(string $id)
-    {
-        //
-    }
 }
